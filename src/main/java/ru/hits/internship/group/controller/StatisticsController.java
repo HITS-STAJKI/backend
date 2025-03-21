5d9e17841442a30f725bd7deac590484c1236da7
Alexey Shumkov <shumleshas@gmail.com>
2025-03-21 14:13:39 +0700

    Добавлен класс контроллера статистики и описание, название

---
+++ src/main/java/ru/hits/internship/group/controller/StatisticsController.java
@@ -0,0 +1,13 @@
+package ru.hits.internship.group.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+@RequestMapping("/api/v1/statistics")
+@Tag(name = "Статистика", description = "Отвечает за работу со статистикой")
+public class StatisticsController {
+}
