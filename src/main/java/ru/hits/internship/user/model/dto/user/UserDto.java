1fcbe45012abc48e4e649006b96de0a35d339b74
Ivan Semenov <devsemtsk@gmail.com>
2025-04-02 03:57:03 +0700

    Remove outdated method

--- src/main/java/ru/hits/internship/user/model/dto/user/UserDto.java
+++ src/main/java/ru/hits/internship/user/model/dto/user/UserDto.java
@@ -1,10 +1,7 @@
 package ru.hits.internship.user.model.dto.user;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import ru.hits.internship.user.model.dto.role.response.RoleDto;
 
 import java.util.*;
@@ -25,13 +22,4 @@ public record UserDto (
 
         @ArraySchema(schema = @Schema(implementation = RoleDto.class, description = "Роль пользователя"))
         Set<RoleDto> roles
-) {
-        @JsonIgnore
-        public Collection<? extends GrantedAuthority> getAuthorities() {
-                return Optional.ofNullable(roles)
-                        .orElse(Collections.emptySet())
-                        .stream()
-                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.userRole().name()))
-                        .toList();
-        }
-}
+) {}
