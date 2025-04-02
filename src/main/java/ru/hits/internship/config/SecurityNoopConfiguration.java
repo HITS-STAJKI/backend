27c284ba6bf84417b48057db70ad2cc7c8a4e879
Alexey Shumkov <shumleshas@gmail.com>
2025-04-02 10:34:36 +0700

    fix security ролей

--- src/main/java/ru/hits/internship/config/SecurityNoopConfiguration.java
+++ src/main/java/ru/hits/internship/config/SecurityNoopConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,7 +21,7 @@ import java.time.LocalDateTime;
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableMethodSecurity
 public class SecurityNoopConfiguration {
 
     private final JwtFilter jwtFilter;
