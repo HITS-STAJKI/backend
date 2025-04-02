d75cd12acd56485739e0b265cdd70aeac5172fa4
Alexey Shumkov <shumleshas@gmail.com>
2025-04-02 11:10:58 +0700

    Подготовка для деплоя под war

--- src/main/java/ru/hits/internship/InternshipApplication.java
+++ src/main/java/ru/hits/internship/InternshipApplication.java
@@ -2,9 +2,16 @@ package ru.hits.internship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InternshipApplication {
+public class InternshipApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InternshipApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(InternshipApplication.class, args);
