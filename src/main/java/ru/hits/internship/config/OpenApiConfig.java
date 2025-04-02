f2945284d0a8ee3852fb762a6660830909502d43
Alexey Shumkov <shumleshas@gmail.com>
2025-04-02 14:33:33 +0700

    swagger fix

--- src/main/java/ru/hits/internship/config/OpenApiConfig.java
+++ src/main/java/ru/hits/internship/config/OpenApiConfig.java
@@ -3,8 +3,14 @@ package ru.hits.internship.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition()
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://tomcat.sonya.jij.li/internship", description = "Production Server (HTTPS)"),
+                @Server(url = "http://localhost:8080", description = "Local Development Server (HTTP)")
+        }
+)
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
