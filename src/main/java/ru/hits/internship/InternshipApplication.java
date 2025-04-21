f2030f7293f6958b73dd62c7465506c02d977379
Alexey Shumkov <shumleshas@gmail.com>
2025-04-21 21:47:04 +0700

    Добавлена зависимость minio, конфигурация с созданием бакета'
    '

--- src/main/java/ru/hits/internship/InternshipApplication.java
+++ src/main/java/ru/hits/internship/InternshipApplication.java
@@ -3,9 +3,11 @@ package ru.hits.internship;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class InternshipApplication extends SpringBootServletInitializer {
 
     @Override
---
+++ src/main/java/ru/hits/internship/config/MinioConfig.java
@@ -0,0 +1,84 @@
+package ru.hits.internship.config;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.concurrent.TimeUnit;
+
+@Getter
+@Setter
+@Slf4j
+@Configuration
+@ConfigurationProperties(prefix = "minio")
+public class MinioConfig {
+    @NotBlank
+    private String endpoint;
+
+    @NotBlank
+    private String accessKey;
+
+    @NotBlank
+    private String secretKey;
+
+    @NotBlank
+    private String bucketName;
+
+    @NotNull
+    private Long connectTimeout;
+
+    @NotNull
+    private Long writeTimeout;
+
+    @NotNull
+    private Long readTimeout;
+
+
+    @Bean
+    public MinioClient minioClient() {
+        try {
+            MinioClient minioClient = MinioClient.builder()
+                    .endpoint(endpoint)
+                    .credentials(accessKey, secretKey)
+                    .httpClient(
+                            new OkHttpClient.Builder()
+                                    .connectTimeout(connectTimeout, TimeUnit.MILLISECONDS)
+                                    .writeTimeout(writeTimeout, TimeUnit.MILLISECONDS)
+                                    .readTimeout(readTimeout, TimeUnit.MILLISECONDS)
+                                    .build()
+                    )
+                    .build();
+
+            boolean bucketExists = minioClient.bucketExists(
+                    BucketExistsArgs.builder().bucket(bucketName).build()
+            );
+
+            if (!bucketExists) {
+                minioClient.makeBucket(
+                        MakeBucketArgs.builder().bucket(bucketName).build()
+                );
+                log.info("Бакет {} создан", bucketName);
+            } else {
+                log.info("Бакет {} уже существует", bucketName);
+            }
+
+            return minioClient;
+        } catch (Exception e) {
+            log.error("Ошибка при создании MinioClient: {}", e.getMessage());
+            throw new RuntimeException("Ошибка при создании MinioClient", e);
+        }
+    }
+
+    @Bean
+    public String bucketName() {
+        return bucketName;
+    }
+}
