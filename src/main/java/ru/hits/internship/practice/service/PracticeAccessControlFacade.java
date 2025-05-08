6b97a4e1a7438cd141db90247893b742bb324b9b
Васильев Данил <danil.a.vasilev@gazprombank.ru>
2025-05-08 14:57:47 +0700

    Добавлена логика для работы с отчетами

--- src/main/java/ru/hits/internship/practice/service/PracticeAccessControlFacade.java
+++ src/main/java/ru/hits/internship/practice/service/PracticeAccessControlFacade.java
@@ -4,6 +4,7 @@ package ru.hits.internship.practice.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.hits.internship.common.exceptions.NotFoundException;
+import ru.hits.internship.practice.entity.PracticeEntity;
 import ru.hits.internship.practice.repository.PracticeRepository;
 import ru.hits.internship.report.entity.ReportEntity;
 
@@ -16,7 +17,7 @@ public class PracticeAccessControlFacade {
 
     public boolean isOwner(UUID userId, UUID practiceId) {
         var practice = practiceRepository.findById(practiceId)
-                .orElseThrow(() -> new NotFoundException(ReportEntity.class, practiceId));
+                .orElseThrow(() -> new NotFoundException(PracticeEntity.class, practiceId));
         UUID authorId = practice.getStudent().getUser().getId();
 
         return authorId.equals(userId);
