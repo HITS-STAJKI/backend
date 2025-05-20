54331aa4e72c4068f9692228f202c9b7c51aef71
Alexey Shumkov <shumleshas@gmail.com>
2025-05-20 10:40:09 +0700

    Исправлены миграции

--- src/main/resources/db/changelog/changesets/V24_drop_comments.sql
+++ src/main/resources/db/changelog/changesets/V24_drop_comments.sql
@@ -1,20 +1,18 @@
 -- liquibase formatted sql
 
 -- changeset shuml:1746699201698-1
-ALTER TABLE report_comments DROP CONSTRAINT fk1i80qi03utu4wxo5vjvs4n4y6;
+-- ALTER TABLE report_comments DROP CONSTRAINT fk1i80qi03utu4wxo5vjvs4n4y6;
 
 -- changeset shuml:1746699201698-2
-ALTER TABLE interview_comments DROP CONSTRAINT fkdyov4q70ks8jtengfjrr90qt2;
+-- ALTER TABLE interview_comments DROP CONSTRAINT fkdyov4q70ks8jtengfjrr90qt2;
 
 -- changeset shuml:1746699201698-3
-ALTER TABLE report_comments DROP CONSTRAINT fki8p0d6l24jm0hokvim8wspptq;
+-- ALTER TABLE report_comments DROP CONSTRAINT fki8p0d6l24jm0hokvim8wspptq;
 
 -- changeset shuml:1746699201698-4
-ALTER TABLE interview_comments DROP CONSTRAINT fkjjuhu54tlgsj8q78na5pgqf9s;
+-- ALTER TABLE interview_comments DROP CONSTRAINT fkjjuhu54tlgsj8q78na5pgqf9s;
 
 -- changeset shuml:1746699201698-5
-DROP TABLE interview_comments CASCADE;
-
--- changeset shuml:1746699201698-6
-DROP TABLE report_comments CASCADE;
+DROP TABLE IF EXISTS interview_comments CASCADE;
+DROP TABLE IF EXISTS report_comments    CASCADE;
 
