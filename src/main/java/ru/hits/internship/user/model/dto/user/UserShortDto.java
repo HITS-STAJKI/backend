f524490f49e9fca968ef9fa375cf46fbdf5c7db5
Ivan Semenov <iva.semenov@tinkoff.ru>
2025-06-01 02:25:23 +0700

    Add lastLoginDate to UserShortDto

--- src/main/java/ru/hits/internship/user/model/dto/user/UserShortDto.java
+++ src/main/java/ru/hits/internship/user/model/dto/user/UserShortDto.java
@@ -3,6 +3,7 @@ package ru.hits.internship.user.model.dto.user;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Schema(description = "Модель пользователя")
@@ -15,6 +16,9 @@ public record UserShortDto (
         @Schema(description = "Адрес электронной почты", example = "example@example.ru")
         String email,
 
+        @NotNull
+        @Schema(description = "Дата и время последнего захода в систему")
+        LocalDateTime lastLoginDate,
 
         @NotNull
         @Schema(description = "Полное имя пользователя", example = "Иванов Иван Иванович")
