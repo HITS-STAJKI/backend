24b967a17ca22fc8b0a24be527162807346aed23
Васильев Данил <danil.a.vasilev@gazprombank.ru>
2025-06-04 14:09:44 +0700

    Правки корсов

--- src/main/java/ru/hits/internship/config/SecurityConfiguration.java
+++ src/main/java/ru/hits/internship/config/SecurityConfiguration.java
@@ -69,8 +69,7 @@ public class SecurityConfiguration {
         );
     }
 
-    @Bean
-    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.addAllowedOrigin("*");
         corsConfig.addAllowedHeader("*");
