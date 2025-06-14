0e29799529df320a3692789a72930d7d84f8e1bd
Alexey Shumkov <shumleshas@gmail.com>
2025-06-14 20:09:20 +0700

    fix

--- src/main/java/ru/hits/internship/statistics/dto/StatisticsResponse.java
+++ src/main/java/ru/hits/internship/statistics/dto/StatisticsResponse.java
@@ -2,8 +2,12 @@ package ru.hits.internship.statistics.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "Статистика по найденным студентам")
 public class StatisticsResponse {
     @Schema(description = "Количество найденных студентов")
