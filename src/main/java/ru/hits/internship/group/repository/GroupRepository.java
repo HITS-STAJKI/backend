a566c329cf24a2c762d80ab40c26e345120ced55
Vasilev Danil <111416100+3eight70@users.noreply.github.com>
2025-06-23 10:37:54 +0700

    Оптимизирован запрос на импорт студентов из таблицы (#46)
    * Оптимизирован запрос на импорт студентов из таблицы
    
    * Переименованы переменные
    
    ---------
    
    Co-authored-by: Васильев Данил <danil.a.vasilev@gazprombank.ru>

--- src/main/java/ru/hits/internship/group/repository/GroupRepository.java
+++ src/main/java/ru/hits/internship/group/repository/GroupRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import ru.hits.internship.group.entity.GroupEntity;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
@@ -15,4 +17,6 @@ public interface GroupRepository extends JpaRepository<GroupEntity, UUID>, JpaSp
     boolean existsByNumberIgnoreCaseAndIdNot(String number, UUID id);
 
     Optional<GroupEntity> findByNumberIgnoreCase(String number);
+
+    List<GroupEntity> findByNumberIn(Set<String> numbers);
 }
--- src/main/java/ru/hits/internship/user/repository/UserRepository.java
+++ src/main/java/ru/hits/internship/user/repository/UserRepository.java
@@ -5,12 +5,17 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ru.hits.internship.common.exceptions.NotFoundException;
 import ru.hits.internship.user.model.entity.UserEntity;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID>, JpaSpecificationExecutor<UserEntity> {
     Boolean existsByEmail(String email);
+
     Optional<UserEntity> findByEmail(String email);
+
+    List<UserEntity> findByEmailIn(Set<String> emails);
 
     default UserEntity findByIdOrThrow(UUID id) {
         return findById(id).orElseThrow(() -> new NotFoundException(UserEntity.class, id));
